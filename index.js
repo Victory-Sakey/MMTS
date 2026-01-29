563a358434d7cdecdc7dded3f40228c6373cc45c
Victory-Sakey <victorysakey66@gmail.com>
2026-01-29 10:28:13 +0100

    done

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // index.js
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve React build folder
-app.use(express.static(path.join(process.cwd(), "build")));
+// Serve static files
+app.use(express.static(path.join(__dirname, "build"))); // CRA
+// app.use(express.static(path.join(__dirname, "dist"))); // Vite
 
 // SPA fallback for React Router
-app.get("*", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "build", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+  // res.sendFile(path.join(__dirname, "dist", "index.html")); // Vite
 });
 
 app.listen(PORT, () => {
